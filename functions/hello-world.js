b5c050e74f027baf89f403225ae1cb3f236fea18
lhamoudi <69698920+lhamoudi@users.noreply.github.com>
2021-09-17 15:07:22 -0700

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello Universe!"
+        message : "Hello World!!"https://github.com/lhamoudi/twilio-runtime-github-actions/blob/main/functions/hello-world.js
     }
 
     callback(null, result);
